4d2de4bd42e6d93160066107f515864682db616b
JURGEN <JURGEN@JURGEN-HP>
2018-01-31 10:18:48 -0500

    ok

--- Captcha-Solver/Captcha-Solver-Lib/MethodsCaptchaSolver.cs
+++ Captcha-Solver/Captcha-Solver-Lib/MethodsCaptchaSolver.cs
@@ -1,5 +1,4 @@
-﻿using AForge.Imaging.Filters;
-using System;
+﻿using System;
 using System.Drawing;
 using Tesseract;
 
@@ -23,6 +22,7 @@ namespace CaptchaSolverLib
                     engine.SetVariable("tessedit_adapt_to_char_fragments", true);
                     engine.SetVariable("tessedit_redo_xheight", true);
                     engine.SetVariable("chop_enable", true);
+
                     Bitmap x = b.Clone(new Rectangle(0, 0, b.Width, b.Height), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
                     using (var page = engine.Process(x, PageSegMode.SingleLine))
